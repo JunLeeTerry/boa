cffa98cb7ae14bd22a5ed11b3633fb5eb8f12dcf
terry_love_c@sina.com <terry_love_c@sina.com>
2016-06-13 22:55:21 +0800

    input form

--- boa/boa/core/forms.py
+++ boa/boa/core/forms.py
@@ -5,11 +5,15 @@ from boa.core.models import Answer
 class AnswerForm(forms.Form):
     name = forms.CharField(required=True,
                            widget=forms.TextInput(
-                               attrs={"class":"form-control",
-                                      "name":"name"}
+                               attrs={"class":"input__field input__field--hoshi",
+                                      "id":"nameinput",
+                                      "name":"name",
+                                      "data-role":"none",}
                            ))
     question = forms.CharField(required=True,
                                widget=forms.TextInput(
-                                   attrs={"class":"form-control",
-                                          "name":"question"}
+                                   attrs={"class":"input__field input__field--hoshi",
+                                          "id":"questioninput",
+                                          "name":"question",
+                                          "data-role":"none",}
                                ))
